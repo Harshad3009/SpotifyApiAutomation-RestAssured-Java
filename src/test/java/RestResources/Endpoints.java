f79b7d7f8e3f5cd90e7b6e5b8a6c0ab6dd76e71c
Harshad3009 <36165491+Harshad3009@users.noreply.github.com>
2025-03-03 18:20:25 +0530

    datafied the user id

--- src/test/java/RestResources/Endpoints.java
+++ src/test/java/RestResources/Endpoints.java
@@ -1,10 +1,12 @@
 package RestResources;
 
+import utils.DataLoader;
+
 public class Endpoints {
 
     public static final String BASEURI = System.getProperty("BASE_URI"); //"https://api.spotify.com"
     public static final String BASEPATH = "/v1";
-    public static final String CREATE_PLAYLIST = "/users/31bp4uhvllxkantyhmp6smjghb4e/playlists";
+    public static final String CREATE_PLAYLIST = "/users/" + DataLoader.getInstance().getData("userId") + "/playlists";
     public static final String PLAYLIST = "/playlists/";
     public static final String ACCOUNTS_BASEURI = System.getProperty("ACCOUNTS_BASE_URI"); //"https://accounts.spotify.com"
     public static final String ACCOUNTS_BASEPATH = "/api";
